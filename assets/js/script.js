dcde2f22025269a8791c15b3dc21cb95a3324776
renzoxst <lorenzoxst@gmail.com>
2021-07-11 15:34:59 -0500

    Password generator actually provides a generated password.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lowCase = upperCase.toLowerCase();
-var specialChars = `" !"#$%&'()*+,-./:;<=>?@^_~"`;
+var specialChars = `"!"#$%&'()*+,-./:;<=>?@^_~"`;
 var numbers = '123456789';
 
 
@@ -11,13 +11,14 @@ lengthPrompt = `What would you like the length of your password to be? \n
 (please enter a number between 8 and 128)`;
 var validLength;
 var passwordLength;
+var newPassword;
+var rand = [];
 
-var passwordArray = [];
 var includeLowerCase;
 var includeUpperCase;
 var includeNums;
 var includeSpecial;
-var randChar;
+
 
 upperCase = Array.from(upperCase);
 lowCase = Array.from(lowCase);
@@ -28,35 +29,55 @@ numbers = Array.from(numbers);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var passwordArray = [];
+  var password = [];
+  generatePassword();
+  password = password.join('');
+  console.log(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  function validateAnswer(confirmAnswer, array1) {
+    if (confirmAnswer === true ) {
+      passwordArray = passwordArray.concat(array1)
+    } else {
+      return;
+    }
+  }
 
-  alert("Lets create a secure password!");
 
-  passwordLength = prompt(lengthPrompt);
-
-  validateNum();
-  
-  validLength = parseInt(passwordLength);
-
-  alert("Now we will pick the criteria of your password. You must pick AT LEAST ONE to generate a password.")
-
-  includes();
-
-  validateAnswer(includeLowerCase, lowCase);
-  validateAnswer(includeUpperCase, upperCase);
-  validateAnswer(includeSpecial, specialChars);
-  validateAnswer(includeNums, numbers);
-
-  console.log(validLength)
-
-  console.log(passwordArray);
 
   function generatePassword(){
+    alert("Lets create a secure password!");
 
+    passwordLength = prompt(lengthPrompt);
+  
+    validateNum();
+    
+    validLength = parseInt(passwordLength);
+    console.log(validLength)
+  
+    alert("Now we will pick the criteria of your password. You must pick AT LEAST ONE to generate a password.")
+  
+    includes();
+  
+    validateAnswer(includeLowerCase, lowCase);
+    validateAnswer(includeUpperCase, upperCase);
+    validateAnswer(includeSpecial, specialChars);
+    validateAnswer(includeNums, numbers);
+  
+    console.log(validLength)
+  
+    console.log(passwordArray.length);
+
+      for (var i = 0; i < validLength; i++ ) {
+      password[i] = passwordArray[Math.floor(Math.random() * passwordArray.length)];
+      console.log(password)
+      }
+      // console.log(rand)
+      // return rand
   };
+
 
 }
 
@@ -68,13 +89,10 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-lengthPrompt = `What would you like the length of your password to be? \n
-(please enter a number between 8 and 128)`;
-
-
+// Functions
 
 function validateNum() {
-  while (parseFloat(passwordLength) < 8 || parseFloat(passwordLength) > 212  || isNaN(passwordLength)) {
+  while (parseFloat(passwordLength) < 8 || parseFloat(passwordLength) > 212  || isNaN(passwordLength) === true || passwordLength === "") {
     alert("You must pick a number between 8 and 128. Please enter a new number");
     passwordLength = prompt(lengthPrompt);
   } 
@@ -99,14 +117,4 @@ function includeAtLeastOne() {
   } 
 }
 
-function validateAnswer(confirmAnswer, array1) {
-  if (confirmAnswer === true ) {
-    passwordArray = passwordArray.concat(array1)
-  } else {
-    return;
-  }
-}
-
-
-// // possible idea, hide password initially, button reveals or hides.
 
